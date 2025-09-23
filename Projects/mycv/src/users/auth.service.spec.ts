1f0c9145942674ef60cdeae36dcfe1cd601f284c
azertyny <ct.foyart@gmail.com>
2025-09-23 20:59:32 +0200

    Refactor TU

--- Projects/mycv/src/users/auth.service.spec.ts
+++ Projects/mycv/src/users/auth.service.spec.ts
@@ -3,26 +3,32 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
-it('can create an instance of auth service', async () => {
-  // create a fake copy of the users service
-  const fakeUserSercice: Partial<UsersService> = {
-    // With partial we don't need to implement all method of UsersService
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
+describe('AuthService', () => {
+  let service: AuthService;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUserSercice,
-      },
-    ],
-  }).compile();
+  beforeEach(async () => {
+    // create a fake copy of the users service
+    const fakeUserSercice: Partial<UsersService> = {
+      // With partial we don't need to implement all method of UsersService
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const service = module.get(AuthService);
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUserSercice,
+        },
+      ],
+    }).compile();
 
-  expect(service).toBeDefined();
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
 });
